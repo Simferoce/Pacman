ff02897cd0c074604f629012c5b446ff3cb3979c
Simon Robidas <le6monstre@hotmail.com>
2017-03-23 10:55:32 -0400

    Fix

--- TPPacman/TPPacman/Program.cs
+++ TPPacman/TPPacman/Program.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
-
->>>>>>> 24fa30ad2696bfc1281fd9578cddd6e4d1522c07
 namespace TPPacman
 {
     class Program
